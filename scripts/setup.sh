4a537ab713d5928c3393df3e7d8b04f0f4082f60
saulbel <97754610+saulbel@users.noreply.github.com>
2023-04-12 11:30:23 +0200

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,7 +4,7 @@
 sudo apt update
 sudo apt install unzip wget -y
 
-# download packer
+# download terraform
 wget https://releases.hashicorp.com/terraform/1.3.5/terraform_1.3.5_linux_amd64.zip
 
 # unzip it
